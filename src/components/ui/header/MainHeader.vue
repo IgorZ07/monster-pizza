dd0f61abd1eb16846efe4c98b3ca7f60c1c35e10
IgorZ07 <wow0707@gmail.com>
2019-10-08 15:10:40 +0200

    Element ui changed to html

--- src/components/ui/header/MainHeader.vue
+++ src/components/ui/header/MainHeader.vue
@@ -1,16 +1,16 @@
 <template>
-  <v-toolbar color="rgba(225,225,225,0)" class="elevation-0" dark fixed app>
-    <v-img
+  <v-toolbar color="rgba(225,225,225,0)" class="elevation-0 toolbar" dark fixed app>
+    <img
       :src="require('../../../assets/monster_pizza_logo_transparent.png')"
       contain
-      height="200"
       class="logo"
-    ></v-img>
+    />
+    <span class="phone-number">095-541-31-91</span>
     <v-toolbar-title class="display-1"></v-toolbar-title>
 
     <v-spacer></v-spacer>
-    <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn v-for="item in mainMenu" :key="item.icon" :to="item.link" flat>
+    <v-toolbar-items class="hidden-sm-and-down links">
+      <v-btn v-for="item in mainMenu" :key="item.icon" :to="item.link" flat large>
         {{
         item.title
         }}
@@ -49,9 +49,47 @@ export default {
 </script>
 
 <style  scoped>
+.toolbar{
+  font-size: 20px;
+}
 .logo {
   position: relative;
-  top: 70px;
-  left: -70px;
+  height: 150px;
+  top: 50px;
+  left: -30px;
+}
+.phone-number {
+  color:rgb(247, 240, 240);
+  font-size: 28px;
+  
+}
+  v-btn {
+    font-size: 40px !important;
+  }
+
+@media screen and (max-width: 992px) {
+  .phone-number {
+      font-size: 20px;
+  }
+  .logo{
+    top: 30px;
+    height: 100px;
+    left: -20px;
+  }
+  .links {
+    font-size: 14px;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .phone-number {
+    min-width:50px;
+    font-size: 12px;
+  }
+  .logo{
+    top: 10px;
+    height: 70px;
+    left: -20px;
+  }
 }
 </style>
