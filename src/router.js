b6ced628c870661042a6c2ad28efd85be2feb957
IgorZ07 <wow0707@gmail.com>
2019-10-08 15:06:02 +0200

    Added route for additional menu layout development

--- src/router.js
+++ src/router.js
@@ -3,10 +3,12 @@ import Router from 'vue-router'
 import Home from './views/index/Home.vue'
 import Menu from './views/menu/Menu.vue'
 import Contact from './views/contact/Contact'
+import About from './views/about/About'
 
 const routes = [
   { path: '/', redirect: '/home' },
   { path: '/home', name: 'home', component: Home },
+  { path: '/about', name: 'about', component: About },
   { path: '/menu', name: 'menu', component: Menu },
   { path: '/contact', name: 'contact', component: Contact }
   // { path: '*', redirect: '/404' }
