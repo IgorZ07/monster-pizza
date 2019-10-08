20e9218fbc367e2de05e5f5a69f75cff21984208
IgorZ07 <wow0707@gmail.com>
2019-10-08 15:08:46 +0200

    Created contact card component

---
+++ src/components/ui/contact/ContactCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="contact-card">
+
+
+
+    </div>
+</template>
+
+<script>
+
+export default {
+name: 'contact-card',
+};
+</script>
+
+<style scoped>
+.contact-card {}
+</style>
