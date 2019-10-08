61d05796e71971a5a66593cae44bd4df6fd24240
IgorZ07 <wow0707@gmail.com>
2019-10-08 15:14:48 +0200

    Contact element updated

--- src/views/contact/Contact.vue
+++ src/views/contact/Contact.vue
@@ -8,6 +8,7 @@
             <div class="resultContainer">
               <v-layout align-center justify-space-around row fill-height>
                 <div class="item"></div>
+
                 <div class="item white">
                   <v-flex>
                     <v-card>
@@ -59,6 +60,65 @@
           </v-flex>
         </v-layout>
       </transition>
+      <!--View-->
+      <v-layout column hidden-md-and-up style="margin-top:20%">
+          <v-flex xs12>
+            <div class="resultContainer">
+              <v-layout align-left justify-space-between column fill-height>
+                <div class="item"></div>
+
+                <div class="item white">
+                  <v-flex>
+                    <v-card>
+                      <v-img :src="require('../../assets/contacts/map.png')"></v-img>
+
+                      <v-img
+                        :src="require('../../assets/contacts/building_sotnya.png')"
+                        aspect-ratio="2.75"
+                      ></v-img>
+                      <v-card-title primary-title>
+                        <div>
+                          <h3 class="headline mb-0">Монстр піцца Поділ</h3>
+                          <div>Полтава, вул. Небесної Сотні, 100</div>
+                          <div>Телефон: +380 95 541 3191</div>
+                        </div>
+                      </v-card-title>
+
+                      <v-card-actions>
+                        <v-btn flat color="orange">
+                          <v-icon>phone</v-icon>Подзвонити
+                        </v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-flex>
+                  <v-flex>
+                    <v-card>
+                      <v-img
+                        :src="require('../../assets/contacts/building_zigina.jpg')"
+                        aspect-ratio="2.75"
+                      ></v-img>
+                      <v-card-title primary-title>
+                        <div>
+                          <h3 class="headline mb-0">Монстр піцца Зигіна</h3>
+                          <div>Полтава, вул. Степана Халтуріна, 11</div>
+                          <div>Телефон: +380 50 134 3797</div>
+                        </div>
+                      </v-card-title>
+
+                      <v-card-actions>
+                        <v-btn flat color="orange">
+                          <v-icon>phone</v-icon>Подзвонити
+                        </v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-flex>
+                </div>
+              </v-layout>
+            </div>
+          </v-flex>
+        </v-layout>
+
+
     </v-container>
   </div>
 </template>
@@ -75,7 +135,7 @@ export default {
 </script>
 
 <style  scoped>
-.about {
+.contact {
   background-image: url("../../assets/background_img/background_hor6.jpg");
   background-size: cover;
 }
